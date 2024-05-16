bdf5ac6e0fadd0944090bb49e968c835357044ed
IOTech-DanielCarneiro <danielcarneiro@iotech.pt>
2024-05-16 13:59:51 +0100

    add login/register to template

--- bandsApp/template/js/views/NavbarView.js
+++ bandsApp/template/js/views/NavbarView.js
@@ -44,6 +44,57 @@ function navbarView() {
   // INJETAR CONTEÚDO NA NAVBAR
   document.querySelector("nav").innerHTML = result;
 
+  // CLICAR NO BOTÃO DE REGISTAR
+  document
+    .querySelector("#frmRegister")
+    ?.addEventListener("submit", (event) => {
+      event.preventDefault();
+      // Gestão do formulário de Registo
+      const registerUsername = document.getElementById("txtUsernameRegister");
+      const registerPassword = document.getElementById("txtPasswordRegister");
+      const registerPassword2 = document.getElementById("txtPasswordRegister2");
+      try {
+        if (registerPassword.value !== registerPassword2.value) {
+          throw Error("Password and Confirm Password are not equal");
+        }
+        User.add(registerUsername.value, registerPassword.value);
+        displayMessage(
+          "msgRegister",
+          "User registered with success!",
+          "success"
+        );
+        // Wait 1 second before reloading, so the user can see the login success message
+        setTimeout(() => {
+          location.reload();
+        }, 1000);
+      } catch (e) {
+        displayMessage("msgRegister", e.message, "danger");
+      }
+    });
+
+  // CLICAR NO BOTÃO DE LOGIN
+  document.querySelector("#frmLogin")?.addEventListener("submit", (event) => {
+    event.preventDefault();
+    try {
+      User.login(
+        document.getElementById("txtUsername").value,
+        document.getElementById("txtPassword").value
+      );
+      displayMessage("msgLogin", "User logged in with success!", "success");
+      // Wait 1 second before reloading, so the user can see the login success message
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
+    } catch (e) {
+      displayMessage("msgLogin", e.message, "danger");
+    }
+  });
+
+  // CLICAR NO BOTÃO LOGOUT (O BOTÃO PODE NÃO EXISTIR POR ISSO USAR "?"" - OPTIONAL CHAINING)
+  document.querySelector("#btnLogout")?.addEventListener("click", () => {
+    User.logout();
+    location.reload();
+  });
 }
 
 function displayMessage(modal, message, type) {
