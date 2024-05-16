2e3bf82d805ceccd0fd62a3759b6032de6c793e3
IOTech-DanielCarneiro <danielcarneiro@iotech.pt>
2024-05-16 13:58:29 +0100

    add login/register to template

--- bandsApp/template/js/init.js
+++ bandsApp/template/js/init.js
@@ -1,59 +1,73 @@
-const bands = [
-  {
-    name: "Muse",
-    genre: "Pop-Rock",
-    cover:
-      "https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/b/9/c/6/b9c663834bfaa66bfc81ab2ac754a628.jpg",
-    desc: "The best band ever",
-    music: "xxx",
-    userId: 1,
-  },
-  {
-    name: "RadioHead",
-    genre: "Pop-Rock",
-    cover:
-      "https://ep01.epimg.net/elpais/imagenes/2017/05/17/icon/1495017818_647155_1495125183_noticia_normal.jpg",
-    desc: "Great songs!",
-    music: "xxx",
-    userId: 1
-  },
-  {
-    name: "James",
-    genre: "Pop-Rock",
-    cover:
-      "http://ksassets.timeincuk.net/wp/uploads/sites/55/2013/01/2012JamesBandPress181212-2.jpg",
-    desc: "The 90's were amazing!",
-    music: "xxx",
-    userId: 2
-  },
-  {
-    name: "Metallica",
-    genre: "Metal",
-    cover:
-      "https://images.impresa.pt/blitz/2016-08-19-metallica.jpg/original/mw-860",
-    desc: "Too much metal for my head",
-    music: "xxx",
-    userId: 1
-  },
-  {
-    name: "Placebo",
-    genre: "Pop-Rock",
-    cover: "https://i.ytimg.com/vi/9aIYBSiAEDk/0.jpg",
-    desc: "Brian Molko is a star!",
-    music: "xxx",
-    userId: 2
-  },
-];
+import * as Band from "./models/BandModel.js";
+import * as User from "./models/UserModel.js";
 
-const users = [
-  {
-    id: 1,
-    username: "user1",
-    password: "pass1",
-  },
-  {
-    id: 2,
-    username: "user2",
-    password: "pass2",
-  },
-];
+initdata();
+
+function initdata() {
+  // BANDS
+  const bands = [
+    {
+      name: "Muse",
+      genre: "Pop-Rock",
+      cover:
+        "https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/b/9/c/6/b9c663834bfaa66bfc81ab2ac754a628.jpg",
+      desc: "The best band ever",
+      music: "xxx",
+      userId: 1,
+    },
+    {
+      name: "RadioHead",
+      genre: "Pop-Rock",
+      cover:
+        "https://ep01.epimg.net/elpais/imagenes/2017/05/17/icon/1495017818_647155_1495125183_noticia_normal.jpg",
+      desc: "Great songs!",
+      music: "xxx",
+      userId: 1
+    },
+    {
+      name: "James",
+      genre: "Pop-Rock",
+      cover:
+        "http://ksassets.timeincuk.net/wp/uploads/sites/55/2013/01/2012JamesBandPress181212-2.jpg",
+      desc: "The 90's were amazing!",
+      music: "xxx",
+      userId: 2
+    },
+    {
+      name: "Metallica",
+      genre: "Metal",
+      cover:
+        "https://images.impresa.pt/blitz/2016-08-19-metallica.jpg/original/mw-860",
+      desc: "Too much metal for my head",
+      music: "xxx",
+      userId: 1
+    },
+    {
+      name: "Placebo",
+      genre: "Pop-Rock",
+      cover: "https://i.ytimg.com/vi/9aIYBSiAEDk/0.jpg",
+      desc: "Brian Molko is a star!",
+      music: "xxx",
+      userId: 2
+    },
+  ];
+
+  // USERS
+  if (!localStorage.users) {
+    const users = [
+      {
+        id: 1,
+        username: "user1",
+        password: "pass1",
+      },
+      {
+        id: 2,
+        username: "user2",
+        password: "pass2",
+      },
+    ];
+    users.forEach((user) => {
+      User.add(user.username, user.password);
+    });
+  }
+}
--- bandsApp/template/js/models/UserModel.js
+++ bandsApp/template/js/models/UserModel.js
@@ -1,1 +1,77 @@
-// UserModel.js
+let users = [];
+
+// CARREGAR UTILIZADORES DA LOCALSTORAGE
+export function init() {
+  if (localStorage.users) {
+    const tempUsers = JSON.parse(localStorage.users);
+    for(let user of tempUsers) {
+      users.push(new User(user.username, user.password));
+    }
+  } else {
+    users = [];
+  }
+}
+
+// ADICIONAR UTILIZADOR
+export function add(username, password) {
+  if (users.some((user) => user.username === username)) {
+    throw Error(`User with username "${username}" already exists!`);
+  } else {
+    users.push(new User(username, password));
+    localStorage.setItem("users", JSON.stringify(users));
+  }
+
+  console.log(users);
+}
+
+// LOGIN DO UTILIZADOR
+export function login(username, password) {
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (user) {
+    sessionStorage.setItem("loggedUser", JSON.stringify(user));
+    return true;
+  } else {
+    throw Error("Invalid login!");
+  }
+}
+
+// LOGOUT DO UTILIZADOR
+export function logout() {
+  sessionStorage.removeItem("loggedUser");
+}
+
+// VERIFICA EXISTÊNCIA DE ALGUÉM AUTENTICADO
+export function isLogged() {
+  return sessionStorage.getItem("loggedUser") ? true : false;
+}
+
+// DEVOLVE UTILZIADOR AUTENTICADO
+export function getUserLogged() {
+  return JSON.parse(sessionStorage.getItem("loggedUser"));
+}
+
+export function findUser(userId) {
+  console.log(users, userId);
+  return users.find((user) => user.id == userId);
+}
+
+function getNextId() {
+  return users.length > 0 ? users.length + 1 : 1;
+}
+
+/**
+ * CLASSE QUE MODELA UM UTILIZADOR NA APLICAÇÃO
+ */
+class User {
+  id = null;
+  username = "";
+  password = "";
+
+  constructor(username, password) {
+    this.id = getNextId();
+    this.username = username;
+    this.password = password;
+  }
+}
